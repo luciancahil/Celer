6eb4ff21784a5a985aadf9620b2f94c21838f29a
unknown <royhe62@yahoo.ca>
2020-12-30 20:35:14 -0800

    added Main and Celer file

---
+++ src/main/CelerNetwork/Celer.java
@@ -0,0 +1,8 @@
+package main.CelerNetwork;
+
+public class Celer {
+
+    public Celer(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+import CelerNetwork.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
